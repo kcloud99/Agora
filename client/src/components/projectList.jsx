6c462ec64d72f70e172536292042b87577a85870
BrandonALittle <brandon.a.little@gmail.com>
2018-02-21 14:23:03 -0800

    created ProjectList component

--- client/src/components/projectList.jsx
+++ client/src/components/projectList.jsx
@@ -4,19 +4,30 @@ class projectList extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-
+      nameVal: '';
     }
   }
 
+  addProject(name) => {
+    console.log(name);
+    this.setState({nameVal: name});
+  }
+
   render() {
+
     var listStyle = {
       height: 60%,
       width: 80%,
       border: solid 1px "gray",
       backgroundColor: "#FFFFFF",
-    }
+    };
+
     return (
+      <div style={listStyle}>
       <div>
+        <input type="text" id="newProjectName">
+        <button id="newProjectButton" onClick={() => this.addProject(document.getElementById("newProjectName").value)}> Add Project </button>
+      </div>
         {props.projects.map(function(project) {
           return <ProjectListEntry project="project"/>
         })}
