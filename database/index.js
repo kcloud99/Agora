49348d974ed1f68409b7a804454e4de2e9454b88
HAQish <mytrueemailaddress@gmail.com>
2018-02-23 13:15:06 -0600

    restarted git process

--- database/index.js
+++ database/index.js
@@ -1,7 +1,6 @@
-// TODO: UNCOMMENT THIS CODE WHEN READY TO CONNECT TO MONGOOSE (lines 3 to 12`)
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test',{useMongoClient: true});
-
+mongoose.connect('mongodb://localhost/Greenfield');
+var Schema = mongoose.Schema;
 var db = mongoose.connection;
 
 db.on('error', function() {
@@ -12,62 +11,33 @@ db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
+var projectSchema = mongoose.Schema({
+    name: String,
+    description: String,
+    customer: String,
+    resources: [{name:String, value:String}],
+    finances: [{name:String, value:String}],
+    timeline: {tasks:[{name:String,dueDate:Date}],start:Date, end:Date, status: String}
+  });
 
+var Projects = mongoose.model('Projects', projectSchema);
 
+var createProject = function(obj){
 
+    var newProject = new Projects(obj);
+    newProject.save(function(err) {
+        if (err) throw err;
+        return 'project successfully saved.';
+    });
+}
 
+var selectAll = function(callback) {
+  return Projects.find();
+};
 
-
-//  THIS IS OLD EXAMPLE CODE THAT SHOULD BE DELETED, FOR EXAMPLE ONLY
-// var itemSchema = mongoose.Schema({
-//   quantity: Number,
-//   description: String
-// },{collection:'item'});
-
-// var Item = mongoose.model('Item', itemSchema);
-
-// var selectAll = function(callback) {
-//   // Item.find({}, function(err, items) {
-//   //   if(err) {
-//   //     callback(err, null);
-//   //   } else {
-//   //     callback(null, items);
-//   //   }
-//   // });
-//    // var item = new Item({
-//    //  quantity: 200,
-//    //  description: 'I am a description!'
-//    // });
-
-//    // item.save(() => {
-//       Item.find({}, function(err,items){
-//         if(err) {
-//           callback(err, null);
-//         } else {
-//           callback(null, items);
-//         }
-//      });
-//    // });
-
-
-// };
-// var save = function(data){
-//   console.log('database mongo---save routine', data);
-//   var item = new Item({
-//       quantity: 200,
-//       description: data.description
-//   });
-//   item.save(function(err) {
-//     if (err) throw err;
-//     console.log('Book successfully saved.');
-//   });
-// }
-//module.exports.save = save;
-
-//module.exports.selectAll = selectAll;
-
-// UNCOMMENT TO TEST SELECTALL
+module.exports.selectAll = selectAll;
+module.exports.createProject = createProject;
 // module.exports.selectAll(function(err, data){
 //   console.log(err);
 //   console.log(data);
-// })
+// });
--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
 var express = require("express"); // using express for routing
-
+var db = require('../database/index.js');
 var app = express(); // initializing app variable for routing
-
+var request = require('request');
 
 var bodyParser = require("body-parser"); // can parse incoming requests and possibly stringify outgoing responses
 app.use(bodyParser.json()); // setting for body parser
 app.use(bodyParser.urlencoded({ extended: false })); // another setting for body parser
 app.use(express.static(__dirname + '/../client/dist')); // location of static files, such as index.html
-var premadeProjects = require("../dummyData.js")
+// var premadeProjects = require("../dummyData.js")
 
 var db = require("../database/index.js"); // assuming we want the database file to be called 'db'
 
 app.get("/projects", function(req, res) { // fetching projects from database
   console.log("Heard get from app.");
-  console.log("Sending hard-coded object, not from actual mongodb database.");
-  res.send(premadeProjects.dummyData); // sending dummy data from static file, not from database
 
+  // console.log("Sending hard-coded object, not from actual mongodb database.");
+  // res.send(premadeProjects.dummyData); // sending dummy data from static file, not from database
 
+  db.selectAll().then( (results) => res.send(results));
   //function, assuming called find, should have tableName.find() somewhere in the body, which returns all of the rows of the table, meaning an array of objects
   //db.find().then(function(rows) res.send(rows));
   //                        ^ this argument should be the return value of the .find function, which should be an array of all of the table row objects
@@ -24,7 +25,21 @@ app.get("/projects", function(req, res) { // fetching projects from database
 
 
 app.post("/projects", function(req, res) { // adding a new project to the database
-  console.log("Heard post from app.");
+
+  // console.log("Heard post from app.======>>>>>>", Object.keys(req.body)[0]);
+  // for(var key in req.body){
+  //   console.log('key IS>>>>>',key);
+  //   console.log('the body of key',req.body[key]);
+  //   var theData = key;
+  //   break;
+  // }
+  // console.log('THE DATA >>>>',theData);
+  // db.createProject(theData);
+  // res.send("Post response.");
+  console.log("Heard post from app.");   /// hello how goes it?
+  // console.log("test",req.body);
+  db.createProject(req.body);
+  res.send(200);
 
   //there should be a function, assuming called save, which takes a single object and saves it into the database via mongoose connection, possibly adding a timestamp field to the row, but the rest of the object schema in the database should match the object key:value pairs in the req.body object
   // db.save(req.body) <= should take the object, map it into a new Collection(obj) (meaning a new document/row in the collection/table) and then .save() the new variable
@@ -55,25 +70,15 @@ app.listen(8080, function() { // creates server - this function runs once upon s
   console.log("Server created, listening on port 8080"); // listening on localhost:8080
 })
 
+// request({
+//     url: "http://localhost:8080/projects",
+//     method: "POST",
+//     json: true,
+//     body: {"name": "Fix a shed", "description": "Fix a shed at the client's house.", "customer": "customer name", "resources": "poreclain", "finances": "450", "timeline": "should finish by next week"}
+// }, function (error, response, body){
+//     console.log("Posted hard-coded request.");
+// });
 
-/* what I imagine the database schema to be for a particular project
-
-{
-    name: String,
-    description: String,
-    customer: customer_id, // from another table
-    resources: [{key: value}, {key: value}, {key: value}],
-    finances: [{key: value}, {key: value}, {key: value}],
-    timeline: {
-      tasks: [{name: "task1", dueDate: "the date that this particular task is due"}, {name: "task2", dueDate: "date that this particular task is due"}],
-      startDate: "the starting date of the project",
-      endDate: "the end date of the project"
-    }
-  }
-}
-
-
-*/
 
 
 
